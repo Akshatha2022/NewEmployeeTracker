3448e3d73f4070f8fae617006d02925646bf1991
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-12 19:08:25 -0500

    correcting typo

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,18 +1,14 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
-USE employee_db;
+USE employees_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  employees_name VARCHAR(100) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT 
+  name VARCHAR(30) NOT NULL,
+   PRIMARY KEY (id)
 );
 
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
-);
+
+
+
