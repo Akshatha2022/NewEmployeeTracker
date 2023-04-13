d5cc3f863efccc2f3a170093958715811951cc49
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-12 19:24:49 -0500

    adding few more to seeds

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO department(name)
-VALUES  ("IT"), 
-        ("Marketing"), 
-        ("Finance"), 
-        ("Sales"), 
-        ("HR");
+INSERT INTO department(name,id)
+VALUES  ('IT',1), 
+        ('Marketing',2), 
+        ('Finance',3), 
+        ('Sales',4), 
+        ('HR',5);
 
 INSERT INTO role(title, salary, department_id)
 VALUES("Engineer", 95000, 1), 
@@ -11,11 +11,13 @@ VALUES("Engineer", 95000, 1),
 ("CFO", 350000, 3), 
 ("Sales Head",280000,4)
 ("Chief Counsel", 300000, 5);
+("Marketing Head", 300000, 2);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ('Aadhrith', 'Ajay', 1, 1), 
-('Akshatha', 'Krishnamurthy', 1, 1), 
-('Abhishek', 'Krishnamurthy', 1, 3), 
-('Girija', 'Nagaraj', 2, 4), 
-('Krishnamurthy', 'Narasimhamurthy', 4, 5);
+('Akshatha', 'Krishnamurthy', 2, 1), 
+('Abhishek', 'Krishnamurthy', 3, 3), 
+('Girija', 'Nagaraj', 5, 4), 
+('Krishnamurthy', 'Narasimhamurthy', 5, 5);
+('Ajay', 'Sridhara',4,2)
        
